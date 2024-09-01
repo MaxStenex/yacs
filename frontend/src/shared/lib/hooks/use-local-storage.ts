824b10319c4fd22f20c577b84e9b2b1a176ca238
Maxim <yourstenex@gmail.com>
2024-09-01 17:18:31 +0300

    feat: use-local-storage hook implementation

---
+++ frontend/src/shared/lib/hooks/use-local-storage.ts
@@ -0,0 +1,57 @@
+"use client";
+import { useCallback, useEffect, useState } from "react";
+
+type SaveValueFn = <T>(value: T) => void;
+
+type ClearValueFn = () => void;
+
+export const useLocalStorage = <T>(
+  key: string,
+  initialValue: T
+): [T, SaveValueFn, ClearValueFn] => {
+  const [value, setValue] = useState(initialValue);
+
+  const saveValue: SaveValueFn = useCallback(
+    (value) => {
+      try {
+        const deserializedValue = JSON.stringify(value);
+        localStorage.setItem(key, deserializedValue);
+      } catch (error) {
+        console.error(`Error in JSON.stringify with key: ${key}`, error);
+      }
+
+      return value;
+    },
+    [key]
+  );
+
+  const clearValue: ClearValueFn = useCallback(() => {
+    localStorage.removeItem(key);
+    setValue(initialValue);
+  }, [initialValue, key]);
+
+  const readSavedValue = useCallback(
+    (key: string): T => {
+      let value: T = initialValue;
+
+      const serializedValue = localStorage.getItem(key);
+      if (serializedValue === null) return value;
+
+      try {
+        value = JSON.parse(serializedValue);
+      } catch (error) {
+        console.error(`Error in JSON.parse with key: ${key}`, error);
+      }
+
+      return value;
+    },
+    [initialValue]
+  );
+
+  useEffect(() => {
+    setValue(readSavedValue(key));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [key]);
+
+  return [value, saveValue, clearValue];
+};
--- frontend/src/shared/lib/index.ts
+++ frontend/src/shared/lib/index.ts
@@ -3,3 +3,4 @@ export { classNames } from "./classnames";
 export { useOutsideClick } from "./hooks/use-outside-click";
 export { useResizeObserver } from "./hooks/use-resize-observer";
 export { useInitialSize } from "./hooks/use-initial-size";
+export { useLocalStorage } from "./hooks/use-local-storage";
